5345343b80e4addc64fee8c12d1b46428e2d76ca
yicr <yicr@users.noreply.github.com>
2026-02-07 08:05:45 +0000

    chore(deps): add my package athena-query-result-pager

--- .projenrc.ts
+++ .projenrc.ts
@@ -13,6 +13,7 @@ const project = new typescript.TypeScriptProject({
   workflowNodeVersion: '24.x',
   deps: [
     '@aws-sdk/client-athena@^3.983.0',
+    'athena-query-result-pager@^0.1.2',
   ],
   depsUpgradeOptions: {
     workflowOptions: {
